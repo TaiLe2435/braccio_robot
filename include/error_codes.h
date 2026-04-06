27ba25d86d7e0bcfecf309a0145f2b0a1edf8937
Tommy Le <TaiLe2435@gmail.com>
2026-04-06 00:10:35 -0700

    include basic error codes

---
+++ include/error_codes.h
@@ -0,0 +1,45 @@
+/*
+ * error_codes.h
+ * Description: Definition of error codes for the application
+ */
+
+#ifndef ERROR_CODES_H
+#define ERROR_CODES_H
+
+/* -- Error Codes ----------------------------------------------------------- */
+typedef enum {
+    STATUS_OK = 0,
+    ERR_INVALID_ARGUMENT,
+    ERR_OPERATION_NOT_PERMITTED,
+    ERR_RESOURCE_UNAVAILABLE,
+    ERR_TIMEOUT,
+    ERR_UNKNOWN
+} StatusCode_t;
+
+#ifdef __cplusplus // if file is being compiled as C++
+extern "C" { // enable C-style linkage
+#endif // prevents name mangling when included in C code
+
+/* -- Includes (STL, ESP-IDF, project) -------------------------------------- */
+
+
+/* -- Enums / Constants ----------------------------------------------------- */
+
+
+/* -- Type definitions (structs, typedefs) ---------------------------------- */
+
+
+/* -- Forward declarations (function prototypes) ---------------------------- */
+
+
+/* -- Class declaration (C++ only) ------------------------------------------ */
+
+
+/* -- C-compatible functions (extern "C" if needed) ------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // ERROR_CODES_H
